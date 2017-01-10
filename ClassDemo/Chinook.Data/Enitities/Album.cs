10b0950c098f502d8ef029dfa792bb6d9977931a
Don Welch <dwelch@nait.ca>
2017-01-09 18:56:58 -0700

    setup entities, DAL, Connectionstrings

---
+++ ClassDemo/Chinook.Data/Enitities/Album.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+#endregion
+
+namespace Chinook.Data.Enitities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        public string Title { get; set; }
+        public int ArtistId { get; set; }
+        public int ReleaseYear { get; set; }
+        public string ReleaseLabel { get; set; }
+
+        //navigational properties
+        public virtual Artist Artist { get; set; }
+    }
+}
---
+++ ClassDemo/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+using Chinook.Data.Enitities;
+#endregion
+namespace ChinookSystem.DAL
+{
+    internal class ChinookContext:DbContext
+    {
+        public ChinookContext():base("ChinookDB")
+        {
+
+        }
+
+        public DbSet<Artist> Artists { get; set; }
+        public DbSet<Album> Albums { get; set; }
+    }
+}
