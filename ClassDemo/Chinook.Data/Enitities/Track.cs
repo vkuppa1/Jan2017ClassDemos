612f88ab4fc422fdf790a6996fd50ab5d8fb3ea7
Don Welch <dwelch@nait.ca>
2017-02-06 19:10:16 -0700

    ListView CRUD setup and Entity Validation

--- ClassDemo/Chinook.Data/Enitities/Track.cs
+++ ClassDemo/Chinook.Data/Enitities/Track.cs
@@ -16,13 +16,23 @@ namespace Chinook.Data.Enitities
     {
         [Key]
         public int TrackId { get; set; }
+
+        [Required(ErrorMessage ="Name is a required field.")]
+        [StringLength(200,ErrorMessage ="Name is limited to 200 characters.")]
         public string Name { get; set; }
         public int? AlbumId { get; set; }
         public int MediaTypeId { get; set; }
         public int? GenreId { get; set; }
+
+        [StringLength(220,ErrorMessage ="Composer is limited to 220 characters.")]
         public string Composer { get; set; }
+
+        [Required(ErrorMessage ="Milliseconds is a required filed")]
+        [Range(1.0,double.MaxValue,ErrorMessage ="Millisecond value out of range; must be greater that 0.")]
         public int Milliseconds { get; set; }
         public int? Bytes { get; set; }
+        [Required(ErrorMessage = "Unit Price is a required filed")]
+        [Range(0.00, double.MaxValue, ErrorMessage = "Unit Price value out of range; must be 0 or greater.")]
         public decimal UnitPrice { get; set; }
 
         //Navigation properties
